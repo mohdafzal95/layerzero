f4c70e6e1c11d02fb35c952e310503ddba6cb533
Afzal_revspire <afzal.mohd@revspire.net>
2024-06-21 19:44:23 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import BlogPost from './Components/BlogPost'; // Assuming you have a separate component for the full blog post
 import Home from './Pages/Home';
-import FallingMeteors from './Components/starts'
 function App() {
   return (
     <div className="App">
@@ -11,7 +9,6 @@ function App() {
        <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/blog/:slug" element={<BlogPost />} />
         </Routes>
       </Router>
     </div>
