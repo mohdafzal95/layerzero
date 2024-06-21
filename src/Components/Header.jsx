1367dc005738b534690cd6c0400a7fe69988d049
Afzal_revspire <afzal.mohd@revspire.net>
2024-06-21 21:48:21 +0530

    fix ui for mobile device

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,37 +1,31 @@
 function Header() {
   return (
-    <main className="flex  pt-8 h-screen text-gray-300  overflow-hidden">
+    <main className="flex  md:pt-8 h-screen text-gray-300  overflow-hidden">
       <div className="flex flex-col w-full">
-        <div className="md:mt-0 md:h-[376px] 2xl:mx-auto  h-[720px] w-[1280px]"
-        >
+        <div className="md:mt-0 2xl:mx-auto  md:h-[620px] w-[1280px] ">
           <video
             muted
             autoPlay
             loop
             playsInline
-            
             className="object-cover md:w-full md:h-ful h-[80%] w-[100%]"
           >
             <source
-                        className=" border-2"
-
+              className=" border-2"
               src="https://claim-layerzero.foundation-8v.xyz/LayerZero+-+Header+V05+-+chrome.webm"
               type="video/webm"
             />
             Your browser does not support the video tag.
           </video>
         </div>
-        <div className="md:py-4  flex flex-col justify-center items-center mx-auto lg:my-auto md:my-0 my-16">
-          <h1 className="font-['Roboto_Mono'] text-xl md:text-4xl md:tracking-widest ">
-            The $ZRO Airdrop has officially
+        <div className="md:py-4  flex flex-col justify-center items-center mx-auto xl:my-auto">
+          <h1 className="font-['Roboto_Mono'] text-4xl  tracking-wider md:tracking-widest mx-12 text-center">
+            The $ZRO Airdrop has officially started!
           </h1>
-          <h1 className="font-['Roboto_Mono'] text-xl md:text-4xl md:tracking-widest">
-            started!
-          </h1>
-          <p className="text-lg md:text-2xl font-['Roboto_Mono'] my-4 md:tracking-widest ">
+          <p className="text-xl md:text-2xl font-['Roboto_Mono'] my-4 md:tracking-widest ">
             Check your eligibility now
           </p>
-          <button className="tracking-widest font-bold font-['Roboto_Mono'] text-lg border rounded-lg py-2.5 px-5 cursor text-black bg-white">
+          <button className="tracking-wide md:tracking-widest font-bold font-['Roboto_Mono'] text-lg border rounded-lg py-2.5 px-5 cursor text-black bg-white">
             Connect Wallet
           </button>
         </div>
