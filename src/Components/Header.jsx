9f8bc2914b82e44d19daeceea7c32cfbe4c09d0e
Afzal_revspire <afzal.mohd@revspire.net>
2024-06-22 10:47:00 +0530

    ui chnages

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,15 @@
 import bgVideo from "../assets/layerzero.mp4"
 function Header() {
   return (
-    <main className="flex  md:pt-8 h-screen text-gray-300  overflow-x-hidden overflow-y-auto">
-      <div className="flex flex-col w-full">
-        <div className="md:mt-0 2xl:mx-auto  md:h-[620px] w-[1280px] ">
+    <main className="flex  md:pt-8 h-screen text-gray-300  overflow-hidden w-full bg-black">
+      <div className="flex flex-col w-full ">
+        <div className="md:mt-0 2xl:mx-auto    w-[900px] md:w-[1280px] ">
           <video
             muted
             autoPlay
             loop
             playsInline
-            className="object-cover md:w-full md:h-ful h-[80%] w-[100%]"
+            className="object-cover md:w-full md:h-ful h-[100%] w-[100%]"
           >
             <source
               className=" border-2"
@@ -19,7 +19,7 @@ function Header() {
             Your browser does not support the video tag.
           </video>
         </div>
-        <div className="md:py-4  flex flex-col justify-center items-center mx-auto xl:my-auto">
+        <div className="md:py-4  flex flex-col justify-center items-center mx-auto my-auto">
           <h1 className="font-['Roboto_Mono'] text-4xl  tracking-wider md:tracking-widest mx-12 text-center">
             The $ZRO Airdrop has officially started!
           </h1>
